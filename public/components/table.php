0225b99101205ad3acb09f485f27b4779affbdb5
jaime_r_pereira <jaime_r_pereira@estudante.sesisenai.org.br>
2026-06-01 12:42:35 -0300

    doc- Table ok e explicada em cada detalhe

--- public/components/table.php
+++ public/components/table.php
@@ -10,14 +10,19 @@
 
     <?php
     
+    // Cria a query SQL para selecionar TODOS os dados e todas as linhas da tabela 'usuarios'
     $sqlTodosUsuarios = "SELECT * FROM usuarios";
 
+    // Executa a query no banco de dados e armazena o conjunto de dados brutos na variável
     $resultadoTodosUsuarios = $conn->query($sqlTodosUsuarios);
 
+    // O loop 'while' vai continuar rodando enquanto houver linhas para ler do banco de dados.
+    // A cada repetição, o 'fetch_assoc()' pega a próxima linha e a transforma em um array chamado $linha
     while($linha = $resultadoTodosUsuarios->fetch_assoc()){
 
     // o fetch assoc
 
+        // Imprime uma nova linha da tabela HTML (<tr>) preenchida dinamicamente com os dados daquela linha do banco
         echo "  <tr>
                     <td>". $linha['id'] . "</td>
                     <td>". $linha['usuario'] . "</td>
