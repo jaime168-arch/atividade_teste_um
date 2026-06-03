feeb2060b085de928cc76a5aaba4fb27d828580a
jaime_r_pereira <jaime_r_pereira@estudante.sesisenai.org.br>
2026-06-03 11:01:35 -0300

    add- 1 e 2 ok parte 3 em andamento

--- excluir.php
+++ excluir.php
@@ -5,3 +5,17 @@ if(!isset($_SESSION["usuario"])){
     header("Location: ../index.php");
     exit();
 }
+
+// Conexão com o Banco
+include("../infra/db/connect.php");
+
+// Verificação de ID passado na URL
+if(isset($_GET['id'])) {
+    // captura do ID que garante ser um número inteiro por segurança~
+    $id_excluir = (int)$_GET['id'];
+
+    // Cria o query SQL para deletar um usuário específico
+    $sqlDelete = "DELETE FROM usuarios WHERE id = $id_excluir";
+
+    //Execução do query
+}
