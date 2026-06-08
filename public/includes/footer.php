558e336d1db937fb4fb24a97d70939ec6dbf9c74
jaime_r_pereira <jaime_r_pereira@estudante.sesisenai.org.br>
2026-06-08 14:49:28 -0300

    add- footer e header

---
+++ public/includes/footer.php
@@ -0,0 +1,6 @@
+<footer>
+        <hr>
+        <p>&copy; 2026 - Sistema Desenvolvido para o Desafio M1.</p>
+    </footer>
+</body>
+</html>
---
+++ public/includes/header.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Login M1</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <h1>Painel de Controle - CRUD</h1>
+        <hr>
+    </header>
